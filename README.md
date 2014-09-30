2100a0a3ada9c5916c920c4178aaa7b9ce3ae49a
Germán Picasso <gpicasso@gmail.com>
2014-09-30 17:42:14 -0500

    Get lines corresponding to Hough peaks

--- README.md
+++ README.md
@@ -6,14 +6,14 @@ Detecting lines in Picasso's [Guernica](http://en.wikipedia.org/wiki/Guernica_(p
 
 ![alt tag](img/guernica.jpg)
 
-1) Gray scale edge detection using the Sobel method
+1) First pass the gray scale version of the image though a Sobel filter to detect all the edges.
 
 ![alt tag](img/guernica_edge.png)
 
-2) [Standar Hugh transform](http://en.wikipedia.org/wiki/Hough_transform) of the detected edges. The color represents the count of points that have given rho (distance from the origin) and theta (angle of the line). The peaks of this map correspond to straight lines in the original image.
+2) [Standar Hugh transform](http://en.wikipedia.org/wiki/Hough_transform) of the detected edges. The color represents the count of points in the edges that have given rho (distance from the origin) and theta (angle of the line). The peaks of this map correspond to straight lines in the original image.
 
 ![alt tag](img/guernica_hough.png)
 
-3) After making it pretty we have 
+3) After making it pretty and squaring the signature we get
 
 ![alt tag](img/guernica_hough_signature.png)
--- hough_transform.m
+++ hough_transform.m
@@ -1,25 +1,60 @@
 addpath('~/Documents/MATLAB/image_tools/')
-% TODO: Mark the maximums in the hough space
 
 I = imread('img/guernica.jpg');
 figure,imshow(I)
 
 %% Get edges
+
 Ig  = rgb2gray(I);
 BW = edge(Ig, 'Sobel');
 figure
 imshow(BW)
 
 %% Line hough transform
+
 [H,theta,rho] = hough(BW,'RhoResolution',0.5,'Theta',-90:0.5:89.5);
 
-figure,
+figure
 imshow(imadjust(mat2gray(H)),'XData',theta,'YData',rho,...
       'InitialMagnification','fit');
 
-xlabel('\theta'), ylabel('\rho');
-axis on, axis normal, hold on;
-colormap(hot);
+xlabel('$\theta$ (degrees)', 'FontSize',30)    
+ylabel('$\rho$ (pixels)', 'FontSize',30)
+axis on normal 
+ylim([-2000 max(rho)])
+set(gca,'FontSize',16)
+colormap(pink);
+
+%% Find peaks
+
+Nlines = 50;
+P = houghpeaks(H, Nlines, 'threshold',ceil(0.3*max(H(:))));
+x = theta(P(:,2));
+y = rho(P(:,1));
+hold on
+plot(x,y,'s','color','black');
+
+%% Plot lines corresponding to peaks
+
+lines = houghlines(BW,theta,rho,P,'FillGap',5,'MinLength',7);
+
+figure, imshow(I), hold on
+max_len = 0;
+for k = 1:length(lines)
+   xy = [lines(k).point1; lines(k).point2];
+   plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
+
+   % Plot beginnings and ends of lines
+   plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
+   plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
+
+   % Determine the endpoints of the longest line segment
+   len = norm(lines(k).point1 - lines(k).point2);
+   if ( len > max_len)
+      max_len = len;
+      xy_long = xy;
+   end
+end
 
 %% Circular hough transform
 
